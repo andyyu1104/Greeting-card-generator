ec80b5de315019b2e58f7a3da7eeb8ee0338bb17
andyyu1104 <andyyu1104@gmail.com>
2025-02-03 17:18:34 +0800

    Improved Welcome Page (2nd ver)

--- frontend/src/pages/Welcome.jsx
+++ frontend/src/pages/Welcome.jsx
@@ -1,39 +1,60 @@
-import { Button, Box, Image } from "@chakra-ui/react";
+import { Button, Box, Image, VStack, Text } from "@chakra-ui/react";
 import { IoStar } from "react-icons/io5";
-import background from "../assets/background/0.png";
+import logo from "../assets/logo/0.png";
+import GCGText from "../assets/logo/1.png";
+import { Link } from "react-router-dom";
 
 const Welcome = () => {
   return (
-    <Box height={"100vh"}>
-      <Button
-        fontSize={["2xl", "3xl", "4xl", "5xl"]}
-        height="fit-content"
-        width="fit-content"
-        padding={"10px"}
-        border="2px"
-        mx={"auto"}
-        display={"flex"}
-        alignItems={"center"}
-        borderColor="#b20838"
-        rightIcon={<IoStar />}
-        position={"absolute"}
-        top={"60%"}
-        left={"50%"}
-        transform={"translate(-50%, -50%)"}
-        color={"#b20838"}
-        _hover={{
-          bg: "#f6a0a6",
-          color: "white",
-        }}
+    <Box height={"100vh"} backgroundColor={"#ff5757"}>
+      <VStack>
+        <Image
+          src={logo}
+          alt="Kirby"
+          objectFit={"contain"}
+          width="50vw"
+          height="25vh"
+        />
+
+        <Image
+          src={GCGText}
+          alt="Greeting Card Generator"
+          objectFit={"contain"}
+          width="50vw"
+          height="25vh"
+        />
+        <Link to={"/create"}>
+          <Button
+            fontSize={["2xl", "3xl", "4xl", "5xl"]}
+            height="fit-content"
+            width="fit-content"
+            padding={"10px"}
+            border="2px"
+            mx={"auto"}
+            display={"flex"}
+            alignItems={"center"}
+            borderColor="#b20838"
+            rightIcon={<IoStar />}
+            color={"#b20838"}
+            _hover={{
+              bg: "#f6a0a6",
+              color: "white",
+            }}
+          >
+            Get started
+          </Button>
+        </Link>
+      </VStack>
+
+      <Text
+        fontSize="3xl"
+        color={"black"}
+        as={"em"}
+        position={"fixed"}
+        bottom={"10px"}
       >
-        Get started
-      </Button>
-      <Image
-        src={background}
-        alt="Greeting Card Generator"
-        width={"100%"}
-        height={"100%"}
-      />
+        @andymantikyu
+      </Text>
     </Box>
   );
 };
