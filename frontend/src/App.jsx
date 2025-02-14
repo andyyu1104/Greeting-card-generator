32a030c25d4334867cff93daeb5051c358037154
andyyu1104 <andyyu1104@gmail.com>
2025-02-14 17:12:21 +0800

    Deployment setting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Create from "./pages/Create";
 import Result from "./pages/Result";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="create" element={<Create />} />
         <Route path="result" element={<Result />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
